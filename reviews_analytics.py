d84f913cf4b8d5bb90c95d88578433fe54610014
xm35p4ru6 <xm35p4ru6@yahoo.com.tw>
2022-08-31 00:57:30 +0800

    更新版

--- reviews_analytics.py
+++ reviews_analytics.py
@@ -6,9 +6,13 @@ with open ('reviews.txt', 'r') as f:
         count += 1
         if count % 1000 == 0: # 如果是1000的倍數就, % 是用來求於數 EX: 7 % 3 = 1, 10 % 6 = 4
             print(len(data)) # 讀取過程中印出進度
-print(len(data)) # 印出 data 計算清單的總數
-print('------------------')
-print(data[0]) # 印出 data 第一筆留言
-print(len(data[0])) # 印出 data 計算第一筆留言的總數
-print('------------------')
-print(data[1]) # 印出 data 第二筆留言
+print('檔案讀取完了, 總共有', len(data), '筆資料') # 印出 data 計算清單的總數
+# print('------------------')
+# print(data[0]) # 印出 data 第一筆留言
+# print(len(data[0])) # 印出 data 計算第一筆留言的總數
+# print('------------------')
+# print(data[1]) # 印出 data 第二筆留言
+sum_len = 0
+for d in data:
+    sum_len = sum_len + len(d)
+print('留言的平均長度為', sum_len/len(data))
